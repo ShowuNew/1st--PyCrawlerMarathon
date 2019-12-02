c0b9fd3f748bebfe36572adb8ec482e639ded139
ShowuNew <fm15gi15@gmail.com>
2019-12-02 22:37:03 +0800

    Update Home3.py
    取檔案找出高雄xml

--- D3/Home3.py
+++ D3/Home3.py
@@ -1,6 +1,6 @@
 import xml.dom.minidom
 import xml.etree.ElementTree as ET
-import xmltodict
+#import xmltodict
 
 ##下載檔案
 # 下載檔案
@@ -15,17 +15,34 @@ import xmltodict
 import os, sys
 
 # 打开文件
+data=[]
 dirs = os.listdir( './data' )
 
 # 输出所有文件和文件夹
-#for file in dirs:
-#    print(file)
-# 讀檔案
-fh = open("./data/64_72hr_CH.xml", "r",encoding="utf-8")
-xml = fh.read()
-fh.close()
 
-print(xml)
+for file in dirs:
+    #print(file)
+    a="./data/"+file
+    #print(os.path.splitext(file)[1])
+    #if os.path.splitext(file)[1]==".xml":
+    #   # if file=="09007_Week24_CH.xml" :
+    #   doc = xml.dom.minidom.parse("./data/"+file)
+    if os.path.splitext(file)[1]==".xml":
+        with open(a, "r",encoding="utf-8") as fdd:
+            xml = fdd.read()
+            if "高雄"  in xml:
+                data.append(file)
+       
+    #print(a)  
+
+print(data) 
+        
+# 讀檔案
+#fh = open("./data/64_72hr_CH.xml", "r",encoding="utf-8")
+#xml = fh.read()
+#fh.close()
+#
+#print(xml)
 ##第一種
 ## 存取檔案
 #doc = xml.dom.minidom.parse("./data/sample.xml")
@@ -54,13 +71,13 @@ print(xml)
 ##第三種
 # 存取檔案
 
-with open('./data/sample.xml', "r",encoding="utf-8") as fd:
-    doc = dict(xmltodict.parse(fd.read()))
-
-# 存取我們的資訊
-print(doc['CUPOY']['Title'])
-
-# 用迴圈存取我們的資訊
-chapters = doc['CUPOY']['Chapters']['Chapter']
-for chapter in chapters:
-    print (chapter['@name'], chapter['#text'])
+#with open('./data/sample.xml', "r",encoding="utf-8") as fd:
+#    doc = dict(xmltodict.parse(fd.read()))
+#
+## 存取我們的資訊
+#print(doc['CUPOY']['Title'])
+#
+## 用迴圈存取我們的資訊
+#chapters = doc['CUPOY']['Chapters']['Chapter']
+#for chapter in chapters:
+#    print (chapter['@name'], chapter['#text'])
