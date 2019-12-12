2479449b5a4875a8c939e3a7523903340a4b705d
ShowuNew <fm15gi15@gmail.com>
2019-12-12 21:32:50 +0800

    D4

--- D4/Home4.py
+++ D4/Home4.py
@@ -1,8 +1,13 @@
 import json
 import requests
 
-r = requests.get('https://cat-fact.herokuapp.com/facts')
+#r = requests.get('https://cat-fact.herokuapp.com/facts')
 
-print(r.text)
+#print(r.text)
 
-print(json.loads(r.text))
+#print(json.loads(r.text))
+
+r=requests.get('http://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json')
+
+r.text
+#print(json.loads(r.text))
