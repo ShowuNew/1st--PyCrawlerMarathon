ad071f0ffc727ad053ac6a1f6c44473de3edd05d
ShowuNew <fm15gi15@gmail.com>
2020-01-05 18:56:39 +0800

    D8

--- D8/home8.py
+++ D8/home8.py
@@ -7,11 +7,11 @@ r.encoding = 'utf-8'
 #print(type(r.text))
 #data = json.loads(r.text)
 
-#b = BeautifulSoup(r.text)
+b = BeautifulSoup(r.text)
 #print(b)
-#print(type(b))
+print(type(b))
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 rr = requests.get('https://www.zhihu.com/explore',headers=headers)
 rr.encoding = 'utf-8'
-print(rr.text[0:600])
+#print(rr.text[0:600])
